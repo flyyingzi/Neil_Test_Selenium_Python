7d2bde95b0b3c49494c33cff4a3341736b2ec0f1
neiltest <niwey2010@hotmail.com>
2014-04-17 14:37:41 +0800

    0417 更新.gitignore文件和修改文件目录

---
+++ learn_python_on_the_hard_way/other/neil_03.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+
+"""
+@Author: Well
+@Date: 2014 - 04 - 17
+"""
+
+
+class MyCase(object):
+    pass
---
+++ selenium_test/check _on_hold/selenium_101.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+"""
+@Author: Well
+@Date: 2014 - 04 - 16
+"""
+
+# 判断文件是否存在，存在的话，直接删除
+
+import os
+
+file_path = r"C:\Users\wei\Documents\GitHub\neilpytest\selenium_test\png\TestCase001.png"
+if os.path.exists(file_path):
+    os.remove(file_path)
+else:
+    print u'文件不存在'
+
